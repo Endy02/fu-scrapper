213d997711055146a45065a116ea06f0d9a78f6d
fujyn <endy.alexis@outlook.com>
2021-12-18 23:00:03 +0100

    Update Setup file
    Added:
    - Package_dir config
    - Readme.md reader for long description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 VERSION = '0.0.1'
 DESCRIPTION = 'NBA data Scrapper for stats analysis'
 
@@ -10,9 +13,12 @@ setup(
     author = 'Fujyn',
     author_email = "fujyn.contact@gmail.com",
     url = 'https://github.com/Endy02/fu-scrapper',
+    project_urls={
+        "Bug Tracker": "https://github.com/Endy02/fu-scrapper/issues",
+    },
     description = DESCRIPTION,
     long_description_content_type = 'text/markdown',
-    long_description = './README.md',
+    long_description=long_description,
     packages = find_packages(),
     install_requires = ['pandas','numpy','request','joblib'],
     python_requires = '>=3.7',
@@ -20,9 +26,12 @@ setup(
     classifiers = [
         'Development status :: 1 - Development',
         'Intended Audience :: Python Developers :: Data Scientist',
-        'Programming Language :: Python :: 3',  
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
         'Operating System :: Unix',
         'Operating System :: MacOs :: MacOS X',
         'Operating System :: Windows',
-    ]
+    ],
+    package_dir={"": "fu-scrapper"},
+    packages=find_packages(where="fu-scrapper"),
 )
