1a95a7c5a8612d6c3f16c7a69fdcf3588cf8cd49
fujyn <endy.alexis@outlook.com>
2021-12-18 22:25:29 +0100

    Create package setup configuration
    -- Add setup.py file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'NBA data Scrapper for stats analysis'
+
+# Setting up the package
+setup(
+    name = "fu-scrapper",
+    version = VERSION,
+    author = 'Fujyn',
+    author_email = "fujyn.contact@gmail.com",
+    url = 'https://github.com/Endy02/fu-scrapper',
+    description = DESCRIPTION,
+    long_description_content_type = 'text/markdown',
+    long_description = './README.md',
+    packages = find_packages(),
+    install_requires = ['pandas','numpy','request','joblib'],
+    python_requires = '>=3.7',
+    keywords = ['python','sports','data-analysis','csv'],
+    classifiers = [
+        'Development status :: 1 - Development',
+        'Intended Audience :: Python Developers :: Data Scientist',
+        'Programming Language :: Python :: 3',  
+        'Operating System :: Unix',
+        'Operating System :: MacOs :: MacOS X',
+        'Operating System :: Windows',
+    ]
+)
